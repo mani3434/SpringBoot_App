9e01148be8aca7be57f15f60c248dfce1da9cc91
mani3434 <S528120@mail.nwmissouri.edu>
2019-01-13 23:56:47 -0500

    springboot starter application added

---
+++ src/main/java/com/mani/earla/SpringBoot_App/SpringBootAppApplication.java
@@ -0,0 +1,14 @@
+package com.mani.earla.SpringBoot_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppApplication.class, args);
+	}
+
+}
+
